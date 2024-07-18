76691272622b32028d9ce5e44fba57af63fff22c
gbpaixao <gustavobezerrapaixao@gmail.com>
2024-07-18 01:06:50 -0300

    test: stubs, spies and mocks implemented

--- test/Signup.test.ts
+++ test/Signup.test.ts
@@ -1,7 +1,7 @@
 
 import { GetAccount } from "../src/application/GetAccount";
 import { Signup } from "../src/application/Signup";
-import { AccountDAOMemory } from "../src/resources/AccountDAO";
+import { AccountDAODatabase, AccountDAOMemory } from "../src/resources/AccountDAO";
 import { MailerGatewayMemory } from "../src/resources/MailerGateway";
 
 let signup: Signup
@@ -10,17 +10,17 @@ let getAccount: GetAccount
 beforeEach(async () => {
   const accountDAO = new AccountDAOMemory()
   const mailerGateway = new MailerGatewayMemory()
-	signup = new Signup(accountDAO, mailerGateway);
+  signup = new Signup(accountDAO, mailerGateway);
   getAccount = new GetAccount(accountDAO)
 })
 
 test("Should create passenger account", async function () {
-	const input = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "87748248800",
-		isPassenger: true
-	};
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248800",
+    isPassenger: true
+  };
   const outputSignup = await signup.execute(input)
   expect(outputSignup).toHaveProperty('accountId')
   const outputGetAccount = await getAccount.execute(outputSignup.accountId)
@@ -31,13 +31,13 @@ test("Should create passenger account", async function () {
 });
 
 test("Should create driver account", async function () {
-	const input = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "87748248800",
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248800",
     carPlate: 'QWE1234',
-		isDriver: true
-	};
+    isDriver: true
+  };
   const outputSignup = await signup.execute(input)
   expect(outputSignup).toHaveProperty('accountId')
   const outputGetAccount = await getAccount.execute(outputSignup.accountId)
@@ -50,61 +50,132 @@ test("Should create driver account", async function () {
 
 
 test("Should throw an error when creating driver account with an invalid car plate", async function () {
-	const input = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "87748248800",
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248800",
     carPlate: 'QWER234',
-		isDriver: true
-	};
+    isDriver: true
+  };
   const output = signup.execute(input)
   await expect(output).rejects.toThrow("Invalid car plate")
 });
 
 test("Should throw an error when creating account with an invalid cpf", async function () {
-	const input = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "87748248801",
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248801",
     carPlate: 'QWE1234',
-		isDriver: true
-	};
+    isDriver: true
+  };
   const output = signup.execute(input)
   await expect(output).rejects.toThrow("Invalid cpf")
 });
 
 test("Should throw an error when creating account with an invalid email", async function () {
-	const input = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}gmail.com`,
-		cpf: "87748248801",
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}gmail.com`,
+    cpf: "87748248801",
     carPlate: 'QWE1234',
-		isDriver: true
-	};
+    isDriver: true
+  };
   const output = signup.execute(input)
   await expect(output).rejects.toThrow("Invalid email")
 });
 
 test("Should throw an error when creating account with an invalid name", async function () {
-	const input = {
-		name: "John",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "87748248801",
+  const input = {
+    name: "John",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248801",
     carPlate: 'QWE1234',
-		isDriver: true
-	};
+    isDriver: true
+  };
   const output = signup.execute(input)
   await expect(output).rejects.toThrow("Invalid name")
 });
 
 test("Should throw an error when creating account with an email already registered", async function () {
-	const input = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "87748248800",
-		isPassenger: true
-	};
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248800",
+    isPassenger: true
+  };
   await signup.execute(input)
   const output = signup.execute(input)
   await expect(output).rejects.toThrow("Account already exists")
+});
+
+test("Should create passenger account com stub", async function () {
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248800",
+    isPassenger: true
+  };
+  const createAccountStub = jest.spyOn(AccountDAODatabase.prototype, 'createAccount').mockResolvedValue(input)
+  const findAccountByEmailStub = jest.spyOn(AccountDAODatabase.prototype, 'findAccountByEmail').mockResolvedValue(null)
+  const findAccountByIdStub = jest.spyOn(AccountDAODatabase.prototype, 'findAccountById').mockResolvedValue(input)
+  const accountDAO = new AccountDAODatabase()
+  const mailerGateway = new MailerGatewayMemory()
+  const signup = new Signup(accountDAO, mailerGateway);
+  const getAccount = new GetAccount(accountDAO)
+  const outputSignup = await signup.execute(input)
+  expect(outputSignup).toHaveProperty('accountId')
+  const outputGetAccount = await getAccount.execute(outputSignup.accountId)
+  expect(outputGetAccount.name).toBe(input.name)
+  expect(outputGetAccount.email).toBe(input.email)
+  expect(outputGetAccount.cpf).toBe(input.cpf)
+  createAccountStub.mockRestore()
+  findAccountByEmailStub.mockRestore()
+  findAccountByIdStub.mockRestore()
+});
+
+test("Should create passenger account com spy", async function () {
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248800",
+    isPassenger: true
+  };
+  const sendSpy = jest.spyOn(MailerGatewayMemory.prototype, 'send')
+  const accountDAO = new AccountDAOMemory()
+  const mailerGateway = new MailerGatewayMemory()
+  const signup = new Signup(accountDAO, mailerGateway);
+  const getAccount = new GetAccount(accountDAO)
+  const outputSignup = await signup.execute(input)
+  expect(outputSignup).toHaveProperty('accountId')
+  const outputGetAccount = await getAccount.execute(outputSignup.accountId)
+  expect(outputGetAccount.name).toBe(input.name)
+  expect(outputGetAccount.email).toBe(input.email)
+  expect(outputGetAccount.cpf).toBe(input.cpf)
+  expect(sendSpy).toHaveBeenCalledTimes(1)
+  expect(sendSpy).toHaveBeenCalledWith(input.email, "Welcome", "")
+  jest.restoreAllMocks();
+});
+
+test.skip("Should create passenger account com mock", async function () {
+  const input = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248800",
+    isPassenger: true
+  };
+  const sendMock = jest.spyOn(MailerGatewayMemory.prototype, 'send').mockImplementation(async (email, subject, body) => { console.log("abc") });
+  const accountDAO = new AccountDAOMemory()
+  const mailerGateway = new MailerGatewayMemory()
+  const signup = new Signup(accountDAO, mailerGateway);
+  const getAccount = new GetAccount(accountDAO)
+  const outputSignup = await signup.execute(input)
+  expect(outputSignup).toHaveProperty('accountId')
+  const outputGetAccount = await getAccount.execute(outputSignup.accountId)
+  expect(outputGetAccount.name).toBe(input.name)
+  expect(outputGetAccount.email).toBe(input.email)
+  expect(outputGetAccount.cpf).toBe(input.cpf)
+  expect(sendMock).toHaveBeenCalledWith(input.email, 'Welcome', '');
+  expect(sendMock).toHaveBeenCalledTimes(1);
+  sendMock.mockRestore()
 });
